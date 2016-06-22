d5b17ea0b01391bd793a1a89592baa4c5eb9914b
unknown <Administrator@SKY-20150711DQF.(none)>
2016-06-22 21:48:47 +0800

    for appManagerPage

--- src/com/mobilesafe/app/AppManagerActivity.java
+++ src/com/mobilesafe/app/AppManagerActivity.java
@@ -8,12 +8,18 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.StatFs;
 import android.text.format.Formatter;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import domain.AppInfo;
@@ -29,6 +35,7 @@ public class AppManagerActivity extends BaseAcitivity {
 
 	private TextView tv_avail_rom;
 	private TextView tv_avail_sd;
+	private TextView tv_show_app_count;
 	private ListView lv_app_manager;
 	
 	private List<AppInfo> appInfos; // 保存所有的应用程序信息
@@ -37,6 +44,7 @@ public class AppManagerActivity extends BaseAcitivity {
 	
 	private LinearLayout ll_loading;
 	private MyAapater adapter;
+	private PopupWindow popupWindow; //弹出的悬浮窗体
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +57,7 @@ public class AppManagerActivity extends BaseAcitivity {
 		userInfos = new ArrayList<AppInfo>();
 		sysInfos = new ArrayList<AppInfo>();
 		
+		tv_show_app_count = (TextView) findViewById(R.id.tv_show_app_count);
 		tv_avail_rom = (TextView) findViewById(R.id.tv_rom);
 		tv_avail_sd = (TextView) findViewById(R.id.tv_sdcard);
 		ll_loading = (LinearLayout) findViewById(R.id.ll_loading_flag);
@@ -79,6 +88,9 @@ public class AppManagerActivity extends BaseAcitivity {
 
 					@Override
 					public void run() {
+						if (userInfos != null) {
+							tv_show_app_count.setText("用户程序 ( "+ userInfos.size() + " )");
+						}
 						lv_app_manager.setAdapter(adapter);
 						ll_loading.setVisibility(View.GONE);
 					}
@@ -86,6 +98,68 @@ public class AppManagerActivity extends BaseAcitivity {
 			}
 		}.start();
 
+		lv_app_manager.setOnScrollListener(new OnScrollListener() {
+
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+			}
+
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
+				dismissPopWindow();
+				
+				if (userInfos != null && sysInfos != null) {
+					if (firstVisibleItem > userInfos.size()) {
+						tv_show_app_count.setText("系统程序 ( " + sysInfos.size()
+								+ " )");
+					} else {
+						tv_show_app_count.setText("用户程序 ( " + userInfos.size()
+								+ " )");
+					}
+				}
+			}
+		});
+		
+		lv_app_manager.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				AppInfo appInfo;
+				if (position == 0 || position == (userInfos.size() + 1)) {
+					return;
+				} else if (position <= userInfos.size()) { //用户程序
+					int newPos = position - 1;
+					appInfo = userInfos.get(newPos);
+				} else { //系统程序
+					int newPos = position - 2 - userInfos.size();
+					appInfo = sysInfos.get(newPos);
+				}
+				showPopupWindow(appInfo, parent, view);
+			}
+
+		});
+	}
+
+	//显示悬浮窗体
+	protected void showPopupWindow(AppInfo appInfo, AdapterView<?> parent, View view) {
+		dismissPopWindow();
+		View contentView = View.inflate(AppManagerActivity.this, R.layout.popup_app_item,
+				null);
+		popupWindow = new PopupWindow(contentView, -2, -2);
+		
+		int[] location = new int[2];
+		view.getLocationInWindow(location);
+		popupWindow.showAtLocation(parent, Gravity.RIGHT | Gravity.TOP,
+				location[0], location[1]);
+	}
+
+	private void dismissPopWindow() {
+		if (popupWindow != null && popupWindow.isShowing()) {
+			popupWindow.dismiss();
+			popupWindow = null;
+		}
 	}
 
 	@SuppressWarnings("deprecation")
@@ -170,5 +244,11 @@ public class AppManagerActivity extends BaseAcitivity {
 		TextView tv_name;
 		TextView tv_location;
 		ImageView iv_icon;
+	}
+	
+	@Override
+	protected void onDestroy() {
+		dismissPopWindow();
+		super.onDestroy();
 	}
 }
